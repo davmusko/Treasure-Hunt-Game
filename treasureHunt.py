389525d3ad09f7f286694d8f4e7ae323495e2239
davmusko <davmusko@gmail.com>
2019-03-19 21:33:42 -0400

    added function to print playing board

--- treasureHunt.py
+++ treasureHunt.py
@@ -5,7 +5,7 @@ import random
 
 print(f"Running {sys.path[0]}.py\nwith Python {sys.version[:5]}\n")
 
-# Returns a matrix. len(list) == 60 && len(list[i] == 15
+# Returns a matrix. grid[x-axis][y-axis]
 # Change ASCII design of grid in this function
 def get_grid():
     grid = []
@@ -16,12 +16,44 @@ def get_grid():
     return grid
 
 
-# Print grid from getNewBoard() with formatting
-def printBoard(grid):
+# Prints playing board and axes from a grid.
+def print_board(grid):
+    # Create a string for the x-axis 10's place
     xAxisTens = "    "
-    for i in range(1,6):
+    for i in range(1, 6):
         xAxisTens += (" " * 9) + str(i)
-    print(xAxisTens)
-    print("    " + ("0123456789" * 6))
 
-printBoard(get_grid())
+    # Create a string for the x-axis 1's place
+    xAxis = ""
+    for i in range(10):
+        xAxis += str(i)
+    xAxis = "   " + (xAxis * 6)
+
+
+    # Prints for x-axes
+    print(xAxisTens)
+    print(xAxis)
+
+
+    # Print each row grid[0][0] + grid[1][0] + grid[2][0]
+    #                grid[0][1] + grid[1][1] + grid[2][1] ect....
+    for row in range(len(grid[0])):
+        # Adds a space for padding to y-axis, single digit numbers
+        if row < 10:
+            padding = " "
+        else:
+            padding = ""
+
+        # Create string for each row.
+        boardRow = ""
+        for column in range(len(grid)):
+            boardRow += grid[column][row]
+
+        print("%s%s %s %s" % (padding, row, boardRow, row))
+
+
+
+
+
+# Below for testing only
+print_board(get_grid())
